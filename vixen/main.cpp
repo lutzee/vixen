72ef30b069ec058244eb68df6bbeac79163b0dd6
Jamie Flint <jamie@lutzee.net>
2016-02-21 17:40:19 +0000

    Initial Commit

---
+++ vixen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vixen
+//
+//  Created by Jamie Flint on 21/02/2016.
+//  Copyright © 2016 Jamie Flint. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
